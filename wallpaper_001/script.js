ac680d17c3d110b09f66bdcc59886885a22092f0
rustyraygun <60489604+rustyraygun@users.noreply.github.com>
2022-08-21 12:16:41 -0500

    Add files via upload

--- wallpaper_001/script.js
+++ wallpaper_001/script.js
@@ -5,11 +5,23 @@ function edit_color_label(value) {
 
 }
 
+function edit_speed_label (value){
+  $("#speed_message").text(value);
+}
+
+
+function slider_range(change_val) {
+  $("#slider_label").text(change_val);
+  livelyPropertyListener("speed", change_val);
+}
 
 
 $(document).ready(function(){
     let intro_message = "Static Blocks Blue"; //project name
     $("#welcome_message").text(intro_message);
+    let slider = document.getElementById("slider_range")
+    let slider_val = String(slider.value);
+    $("#slider_label").text(slider_val);
     
  
 });
@@ -27,13 +39,10 @@ function livelyPropertyListener(name, val)
       window.localStorage.setItem("color_selected", val);
       edit_color_label(val);
     }
-    else if(name =="btnUpdate") 
+    else if(name =="speed") 
     {
-      window.localStorage.setItem("updated", val);
-      let msg = "Updated";
-      $("#welcome_message").text(msg);
+      window.localStorage.setItem("speed_selected", val);
+      //edit_speed_label(val);
     }
-    else {
-      window.localStorage.setItem("color_selected", null);
-    }
+    
 }
